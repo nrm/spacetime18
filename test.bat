52f02d9b82fddbd0c7488c776f630f88213baf6e
ksl <kurdubov@iaaras.ru>
2024-06-09 20:19:03 +0300

    update paths and gitignore

--- test.bat
+++ test.bat
@@ -1,11 +1,10 @@
- setlocal
+setlocal
 
- set PYTHON_PATH=C:\Users\Yegorius\Downloads\VSCode_cpp_py\VSCode\Code.exe\..\Python\Python310\python.exe
+set PYTHON_PATH=python.exe
 
- set SCRIPT_PATH=c:\Users\Yegorius\Desktop\hack\images\process_image.py
+set SCRIPT_PATH=process_image.py
 
- set SUBSTRATE_ORIGINAL_PATH=layouts\layout_2021-08-16.tif
-@REM  %PYTHON_PATH% %SCRIPT_PATH% %SUBSTRATE_ORIGINAL_PATH%
+set SUBSTRATE_ORIGINAL_PATH=layouts\layout_2021-08-16.tif
 
 set SUBSTRATE_PATH=layouts\layout_2021-06-15.tif
  %PYTHON_PATH% %SCRIPT_PATH% %SUBSTRATE_PATH%
@@ -15,3 +14,5 @@ set SUBSTRATE_PATH=layouts\layout_2022-03-17.tif
  %PYTHON_PATH% %SCRIPT_PATH% %SUBSTRATE_PATH%
 
 endlocal
+
+pause
