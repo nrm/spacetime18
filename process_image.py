7bbe1cbfce795d3fc868b733c22140d01110df22
ksl <kurdubov@iaaras.ru>
2024-06-14 18:18:20 +0300

    work with colors

--- process_image.py
+++ process_image.py
@@ -90,7 +90,7 @@ def transform_and_fill_new(F, mult_x=5.,mult_y=9,angle=15):
 
 def cross_correlate_2d(x, h):
     h = np.fft.ifftshift(np.fft.ifftshift(h, axes=0), axes=1)
-    return np.fft.ifft2(np.fft.fft2(x) * np.conj(np.fft.fft2(h)))
+    return np.fft.ifft2(np.fft.fft2(x,axes=(0,1)) * np.conj(np.fft.fft2(h,axes=(0,1))),axes=(0,1))
 
 def ccf_repro_images(diff_crop,cropped_substrate,ncut):
     mcrop=np.zeros(cropped_substrate.shape)
@@ -152,14 +152,15 @@ def ccf_repro_images_fullHD(diff_crop, cropped_substrate, ncut):
             yc = cropped_substrate.shape[1] // 2 - cs[1]//2
             
             try:
-                mcrop[xc:xc + cs[0], yc:yc + cs[1]] = diff_crop[i * cs[0]:(i + 1) * cs[0], j * cs[1]:(j + 1) * cs[1]]
+                mcrop[xc:xc + cs[0], yc:yc + cs[1],:] = diff_crop[i * cs[0]:(i + 1) * cs[0], j * cs[1]:(j + 1) * cs[1],:]
             except:
                 continue
 
             ccf = np.abs(cross_correlate_2d(mcrop, cropped_substrate))  # !!!!!!
-
+            ccf = np.sum(ccf[:,:,:3],axis=2)
             x, y = np.unravel_index(ccf.argmax(), ccf.shape)
             snr = np.max(ccf) / np.mean(ccf)
+
             #print(i, j, x, y, snr)
 #            if snr > 10:
             if (snr > 8) and \
@@ -167,8 +168,8 @@ def ccf_repro_images_fullHD(diff_crop, cropped_substrate, ncut):
                 np.abs(y + j * cs[1] - cropped_substrate.shape[1] + cs[1]//2)<cs[1]:
                     try:
                         res[xc * 2 + cs[0] // 2 - x:xc * 2 + cs[0] // 2 - x + cs[0],
-                        yc * 2 + cs[1] // 2 - y:yc * 2 + cs[1] // 2 - y + cs[1]] = diff_crop[i * cs[0]:(i + 1) * cs[0],
-                                                                        j * cs[1]:(j + 1) * cs[1]]
+                        yc * 2 + cs[1] // 2 - y:yc * 2 + cs[1] // 2 - y + cs[1],:] = diff_crop[i * cs[0]:(i + 1) * cs[0],
+                                                                        j * cs[1]:(j + 1) * cs[1],:]
                         crop_coords.append((i * cs[0], j * cs[1]))
                         cropped_substrate_coords.append((xc * 2 + cs[0] // 2 - x, yc * 2 + cs[1] // 2 - y))
                         #print(snr,x + i * cs[0] - cropped_substrate.shape[0] + cs[0]//2, y + j * cs[1] - cropped_substrate.shape[1] + cs[1]//2)
@@ -194,6 +195,12 @@ def cloud_filter(data0):
     data=data0*1
     data[1:,:]=np.where(np.abs(data[:-1,:])>0,data[1:,:],0)
     data[:,1:]=np.where(np.abs(data[:,:-1])>0,data[:,1:],0)
+    data[:-1,:]=np.where(np.abs(data[1:,:])>0,data[:-1,:],0)
+    data[:,:-1]=np.where(np.abs(data[:,1:])>0,data[:,:-1],0)
+    data[2:,:]=np.where(np.abs(data[:-2,:])>0,data[2:,:],0)
+    data[:,2:]=np.where(np.abs(data[:,:-2])>0,data[:,2:],0)
+    data[:-2,:]=np.where(np.abs(data[2:,:])>0,data[:-2,:],0)
+    data[:,:-2]=np.where(np.abs(data[:,2:])>0,data[:,:-2],0)
     return data
 
 def smooth_(y, box_pts):
@@ -277,8 +284,51 @@ def make_derivative(data0,mult_x,mult_y,result_type='x'):
         return data - np.mean(data)
     return 'unknown data type'
 
+def calc_maxcoin(coins):
+    tolerance=5
+    for i in range(coins.shape[0]):
+        coin=0
+        for j in range(coins.shape[0]):
+            if i==j:
+                continue
+            if abs(coins[i][0]-coins[j][0])+abs(coins[i][1]-coins[j][1])<tolerance:
+                coin+=1
+        coins[i,3]=coin
+    return np.argmax(coins[:,3]),coins
 
-def calc_for_mults_new(diff_crop0,substrate,mult_i,mult_j,deriv_type,return_type='snr'):
+
+def argmax_ignore_bounds(array, stepx, stepy):
+    """
+    Find the argmax index in a 2D array ignoring boundary regions.
+
+    Parameters:
+    array (np.ndarray): 2D array in which to find the argmax.
+    stepx (int): Number of rows to ignore from the start and end.
+    stepy (int): Number of columns to ignore from the start and end.
+
+    Returns:
+    tuple: Tuple representing the argmax index within the valid region of the array.
+    """
+    rows, cols = array.shape
+
+    # Ensure stepx and stepy are valid
+    if stepx * 2 >= rows or stepy * 2 >= cols:
+        raise ValueError("Step sizes are too large for the array dimensions.")
+
+    # Slice the array to exclude the boundaries
+    valid_region = array[stepx:rows-stepx, stepy:cols-stepy]
+
+    # Find the index of the maximum value in the valid region
+    local_argmax = np.unravel_index(np.argmax(valid_region), valid_region.shape)
+
+    # Map local indices to global indices
+    global_argmax =  local_argmax[1] + stepy,local_argmax[0] + stepx
+
+    return global_argmax
+
+
+
+def calc_for_mults_new(diff_crop,substrate,mult_i,mult_j,deriv_type,return_type='snr',find_rotation=False):
 #    diff_substrate = substrate[::mult_i, ::mult_j] * 1.0
 
 #    diff_substrate[1:,:]=diff_substrate[:-1,:]-diff_substrate[1:,:]
@@ -286,16 +336,22 @@ def calc_for_mults_new(diff_crop0,substrate,mult_i,mult_j,deriv_type,return_type
 #    diff_substrate[0,:]=np.zeros(diff_substrate.shape[1])
 #    print(mult_j,mult_i)
 #     print(substrate.shape)
-    angles=np.arange(-1.5,1.5,0.2)
+    if find_rotation:
+        angles=np.arange(-1.5,1.5,0.2)
+    else:
+        angles=[0]
     #angles=[0]
-    result=(0,0,0,0,0)
-    diff_substrate=make_derivative(substrate,mult_i,mult_j,deriv_type)
+    result=(0,0,0,0,0,0,0)
+    #diff_substrate=make_derivative(substrate,mult_i,mult_j,deriv_type)
     #plt.imshow(diff_substrate)
     #plt.show()
     for angl in angles:
-        #transf_sub=transform_and_fill_new(substrate,mult_j,mult_i,angle=angl)
-        #diff_substrate=make_derivative(transf_sub,1,1,deriv_type)
-        diff_crop=transform_and_fill_new(diff_crop0,1,1,angle=angl)
+        if find_rotation:
+            transf_sub=transform_and_fill_new(substrate,mult_j,mult_i,angle=angl)
+            diff_substrate=make_derivative(transf_sub,1,1,deriv_type)
+        else:
+            diff_substrate=make_derivative(substrate,mult_i,mult_j,deriv_type)
+        #diff_crop=transform_and_fill_new(diff_crop0,1,1,angle=angl)
         # print(diff_substrate.shape)
         # exit(0)
 
@@ -308,17 +364,38 @@ def calc_for_mults_new(diff_crop0,substrate,mult_i,mult_j,deriv_type,return_type
 #        print(im1[ix // 2:ix // 2 + i1mx, iy // 2:iy // 2 + i1my].shape,diff_crop.shape)
         im1[(ix- i1mx) // 2:(ix- i1mx) // 2 + i1mx, (iy-i1my) // 2:(iy-i1my) // 2 + i1my,:] = diff_crop
         ccf = np.abs(cross_correlate_2d(im1, diff_substrate))  # !!!!!!
-        ccf = np.sum(ccf,axis=2)
-        plt.imshow(ccf)
-        plt.show()
-#        for color in range(ccf.shape[2]):
-#            x, y = np.unravel_index(ccf[:,:,color].argmax(), ccf.shape[:2])
-#            snr = np.max(ccf[:,:,color]) / np.mean(ccf[:,:,color])
-#            print(color,snr)
+        coins=np.zeros((ccf.shape[2],4))
+        for color in range(ccf.shape[2]):
+            x, y = np.unravel_index(ccf[:,:,color].argmax(), ccf.shape[:2])
+#            x, y = argmax_ignore_bounds(ccf[:,:,color],i1mx//2,i1my//2)
+            snr = np.max(ccf[:,:,color]) / np.mean(ccf[:,:,color])
+            coins[color,0] = x
+            coins[color,1] = y
+            coins[color,2] = snr
+
+ #           print(color,x,y,snr)
+
+    #    ccf = np.sum(ccf,axis=2)
+        maxcoin_arg,coins = calc_maxcoin(coins)
+        maxcoin=coins[maxcoin_arg,3]
+        ccf = np.sum(ccf[:,:,:3],axis=2)
+#        ccf = ccf[:,:,3]
+
+    #    plt.imshow(ccf)
+    #    plt.show()
         x, y = np.unravel_index(ccf.argmax(), ccf.shape)
+        #x, y = argmax_ignore_bounds(ccf,i1my//2,i1mx//2)
         snr = np.max(ccf) / np.mean(ccf)
-        if result[0]<snr:
-            result=(snr,mult_i,mult_j,x,y,angl)
+#        print('sum snr :',snr,mult_i,mult_j,x,y)
+
+#        if result[0]<snr:
+#            result=(snr,mult_i,mult_j,x,y,angl,maxcoin)
+
+        if result[6]<=maxcoin:
+            if result[0]<snr:
+                result=(snr,mult_i,mult_j,int(coins[maxcoin_arg,0]),int(coins[maxcoin_arg,1]),angl,maxcoin)
+#                result=(snr,mult_i,mult_j,x,y,angl,maxcoin)
+
 
     if return_type=='snr':
         #print((snr,mult_i,mult_j))
@@ -328,7 +405,7 @@ def calc_for_mults_new(diff_crop0,substrate,mult_i,mult_j,deriv_type,return_type
         return ccf
 
 
-def calc_for_mults(diff_crop,substrate,mult_i,mult_j,deriv_type,return_type='snr'):
+def calc_for_mults(diff_crop,substrate,mult_i,mult_j,deriv_type,return_type='snr',tmp=False):
 #    diff_substrate = substrate[::mult_i, ::mult_j] * 1.0
 
 #    diff_substrate[1:,:]=diff_substrate[:-1,:]-diff_substrate[1:,:]
@@ -345,12 +422,29 @@ def calc_for_mults(diff_crop,substrate,mult_i,mult_j,deriv_type,return_type='snr
         im1 = np.zeros(diff_substrate.shape,dtype=complex)
     else:
         im1 = np.zeros(diff_substrate.shape)
-    i1mx, i1my = diff_crop.shape
+    i1mx, i1my = diff_crop.shape[:2]
     # print(im1[ix // 2:ix // 2 + i1mx, iy // 2:iy // 2 + i1my].shape,diff_crop.shape)
     im1[(ix- i1mx) // 2:(ix- i1mx) // 2 + i1mx, (iy-i1my) // 2:(iy-i1my) // 2 + i1my,:] = diff_crop
+
+
     ccf = np.abs(cross_correlate_2d(im1, diff_substrate))  # !!!!!!
+    #plt.imshow(ccf,vmax=4*np.mean(np.abs(ccf)))
+    #plt.show()
+    for color in range(ccf.shape[2]):
+        x, y = np.unravel_index(ccf[:,:,color].argmax(), ccf.shape[:2])
+        snr = np.max(ccf[:,:,color]) / np.mean(ccf[:,:,color])
+        print(color,x,y,snr)
+
+#    ccf = np.sum(ccf,axis=2)
+    #ccf = np.sum(ccf[:,:,:3],axis=2)
+    ccf = ccf[:,:,3]
+
+#    plt.imshow(ccf)
+#    plt.show()
     x, y = np.unravel_index(ccf.argmax(), ccf.shape)
     snr = np.max(ccf) / np.mean(ccf)
+    print('sum snr :',snr,mult_i,mult_j,x,y)
+
     if return_type=='snr':
         #print((snr,mult_i,mult_j))
         return (snr,mult_i,mult_j,x,y)
@@ -359,27 +453,32 @@ def calc_for_mults(diff_crop,substrate,mult_i,mult_j,deriv_type,return_type='snr
 
 
 
-def initial_search(diff_crop, substrate, mults,deriv_type):
+def initial_search(diff_crop, substrate, mults,deriv_type,find_rotation=False):
     #best_ccf=np.zeros(diff_crop.shape)
     import time
 
     start = time.time()
     best_snr=0
+    best_coin=0
     optm=(0,0)
     parlist=[]
     for mult_i in mults[0]:
         for mult_j in mults[1]:
-            parlist.append((diff_crop,substrate,mult_i,mult_j,deriv_type))
+            parlist.append((diff_crop,substrate,mult_i,mult_j,deriv_type,'snr',find_rotation))
 
-    snrs=Parallel(n_jobs=8)(delayed(calc_for_mults_new)(*i) for i in parlist)
-#    snrs=Parallel(n_jobs=8)(delayed(calc_for_mults)(*i) for i in parlist)
+    snrs=Parallel(n_jobs=16)(delayed(calc_for_mults_new)(*i) for i in parlist)
+#    snrs=Parallel(n_jobs=1)(delayed(calc_for_mults)(*i) for i in parlist)
     ii=0
     for mult_i in mults[0]:
         for mult_j in mults[1]:
             snr=snrs[ii][0]
-            if snr > best_snr:
-                optm = snrs[ii][1:]
-                best_snr = snr
+            coin=snrs[ii][6]
+            #print(snr)
+            if coin >= best_coin:
+                if snr > best_snr:
+                        optm = snrs[ii][1:]
+                        best_snr = snr
+                        best_coin = coin
             ii += 1
     #print("hello")
     end = time.time()
@@ -418,16 +517,16 @@ def angle_test_fullHD(diff_crop, substrate, angls, mult_i,mult_j, deriv_type):
 #def extrapolate_crop(crop,mx,my):
 def scale_image(image, multx, multy):
     # Step 1: Perform 2D FFT to transform the image to the frequency domain
-    f_transform = np.fft.fft2(image)
+    f_transform = np.fft.fft2(image,axes=(0,1))
 
     # Step 2: Shift the zero-frequency component to the center
-    f_transform_shifted = np.fft.fftshift(f_transform)
+    f_transform_shifted = np.fft.fftshift(f_transform,axes=(0,1))
 
     # Step 3: Get the dimensions of the original image
     original_shape = image.shape
 
     # Step 4: Calculate the new shape based on scaling factors
-    new_shape = (int(original_shape[0] * multy), int(original_shape[1] * multx))
+    new_shape = (int(original_shape[0] * multy), int(original_shape[1] * multx),original_shape[2])
 
     # Step 5: Create a new array with the new shape and insert the frequency domain data
     f_transform_resized = np.zeros(new_shape, dtype=complex)
@@ -447,10 +546,10 @@ def scale_image(image, multx, multy):
         center_x_orig - min_x // 2:center_x_orig + min_x // 2]
 
     # Step 6: Shift back the zero-frequency component to the original place
-    f_transform_resized_shifted_back = np.fft.ifftshift(f_transform_resized)
+    f_transform_resized_shifted_back = np.fft.ifftshift(f_transform_resized,axes=(0,1))
 
     # Step 7: Perform the inverse 2D FFT to transform back to the spatial domain
-    scaled_image = np.fft.ifft2(f_transform_resized_shifted_back)
+    scaled_image = np.fft.ifft2(f_transform_resized_shifted_back,axes=(0,1))
 
     return scaled_image
 
@@ -474,13 +573,14 @@ def process_crop(crop, crop_file_name, substrate, mults):
     #diff_crop[0, :]=np.zeros(diff_crop.shape[1])
     diff_crop = make_derivative(med_crop,1,1,deriv_type)
 
-    ISmult=5
+    ISmult=2
     med_crop_sm = smooth(med_crop,ISmult,ISmult)
     diff_crop_sm = make_derivative(med_crop_sm,ISmult,ISmult,deriv_type)
     substrate_sm = smooth(substrate,ISmult*6,ISmult*6)
     substrate_sm = substrate_sm[::ISmult,::ISmult]
 #    try:
-    optm,best_snr = initial_search(diff_crop_sm, substrate_sm, mults, deriv_type)
+    optm,best_snr = initial_search(diff_crop_sm, substrate_sm, mults, deriv_type,find_rotation=False)
+#    optm,best_snr = initial_search(diff_crop, substrate_sm, mults, deriv_type)
 #    except:
 #        print('Initial search failed')
 #        return [],[],[]
@@ -512,9 +612,9 @@ def process_crop(crop, crop_file_name, substrate, mults):
     if ShowPlot:
         fig = plt.figure()
         fig.add_subplot(1, 2, 1)
-        plt.imshow(np.abs(diff_crop))
+        plt.imshow(np.sum(np.abs(diff_crop),axis=2))
         fig.add_subplot(1, 2, 2)
-        plt.imshow(np.abs(cropped_substrate),vmax=10*np.median(np.abs(cropped_substrate)))
+        plt.imshow(np.sum(np.abs(cropped_substrate),axis=2))
         fig.suptitle('Метод производной для загрубленной подложки 1')
         plt.show()
 
@@ -532,17 +632,17 @@ def process_crop(crop, crop_file_name, substrate, mults):
     #opt_ang = angle_test_fullHD(diff_crop, cropped_substrateHD, angls, optm[0], optm[1], deriv_type)
     #exit(0)
 
-    addmult=0.1
-    new_mults=[np.arange(optm[0]-addmult,optm[0]+addmult,0.1),np.arange(optm[1]-addmult,optm[1]+addmult,0.1)]
+    addmult=0.5
+    new_mults=[np.arange(optm[0]-addmult,optm[0]+addmult,0.2),np.arange(optm[1]-addmult,optm[1]+addmult,0.2)]
 #    new_mults = [np.arange(optm[0] - 0.6, optm[0] + 0.6, 0.1), np.arange(optm[1] - 0.6, optm[1] + 0.6, 0.1)]
 #    new_mults=[np.arange(optm[0]-0.1,optm[0]+0.1,0.1),np.arange(optm[1]-0.1,optm[1]+0.1,0.1)]
 
     #new_mults=[[optm[0]],[optm[1]]]
-    try:
-        optm, snr_refined = initial_search(diff_crop, cropped_substrateHD, new_mults, deriv_type)
-    except:
-        print('refined search failed')
-        return [],[],[]
+    #try:
+    optm, snr_refined = initial_search(diff_crop, cropped_substrateHD, new_mults, deriv_type,find_rotation=True)
+    #except:
+    #    print('refined search failed')
+    #    return [],[],[]
 
     print('optm1:',optm, ' SNR_refined:',snr_refined)
     if snr_refined < best_snr:
@@ -624,7 +724,8 @@ if __name__ == "__main__":
         plt.show()
     
 #    mults=[[5,6,7,8,9,10],[5,6,7,8,9,10]]
-    mults=np.array([np.arange(5,10,0.5),np.arange(5,10,0.5)])
+    mults=np.array([np.arange(9,11,0.5),np.arange(5,7,0.5)])
+#    mults=np.array([np.arange(5,10,0.5),np.arange(5,10,0.5)])
     #mults=np.arange(5,10,0.4)
     if bSaveLog:
         log_file.write('Layout: {}\n'.format(args.substrate_path))
