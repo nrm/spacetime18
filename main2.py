ff009a1377011acf654906eca5caf6cb97651491
Bezrukov Ilya <bezrukov@iaaras.ru>
2024-06-16 08:37:10 +0300

    test API

---
+++ main2.py
@@ -0,0 +1,100 @@
+from fastapi import FastAPI, File, UploadFile, HTTPException, BackgroundTasks
+from fastapi.responses import FileResponse, JSONResponse
+from pydantic import BaseModel
+import os
+from pixel_repair_report import process_image_file
+from process_image import main_process_func
+from datetime import datetime, timezone
+
+app = FastAPI()
+
+# Директория, в которой будут сканироваться подложки
+LAYOUTS_DIR = os.getenv("LAYOUTS_DIR", "/layouts")
+
+# Хранение статусов задач
+task_status = {}
+
+class Layout(BaseModel):
+    name: str
+    description: str
+
+# Функция для сканирования директории и получения списка подложек
+def scan_layouts(directory: str):
+    tif_files = [f for f in os.listdir(directory) if f.endswith('.tif')]
+    layouts = [{"name": f, "description": f"Description of {f}"} for f in tif_files]
+    return layouts
+
+# Сканирование директории при запуске приложения
+layouts = scan_layouts(LAYOUTS_DIR)
+
+@app.get("/layouts/")
+async def get_layouts():
+    return layouts
+
+@app.post("/process_image/")
+async def process_image(file: UploadFile = File(...)):
+    input_file_path = f"temp_{file.filename}"
+    output_file_path = f"fixed_{file.filename}"
+
+    with open(input_file_path, "wb") as buffer:
+        buffer.write(await file.read())
+
+    hot_pixel_report = process_image_file(input_file_path, output_file_path)
+
+    os.remove(input_file_path)
+
+    report_file_path = f"report_{file.filename}.txt"
+    with open(report_file_path, "w") as report_file:
+        report_file.write("Отчет о битых пикселях:\n")
+        for line in hot_pixel_report:
+            report_file.write(line + "\n")
+
+    return {
+        "fixed_image": output_file_path,
+        "report": report_file_path
+    }
+
+@app.get("/download_fixed_image/{filename}")
+async def download_fixed_image(filename: str):
+    return FileResponse(filename)
+
+@app.get("/download_report/{filename}")
+async def download_report(filename: str):
+    return FileResponse(filename)
+
+def run_main_process(layout_name: str, input_file_path: str, taskid: str):
+    layout_name = os.path.join(LAYOUTS_DIR, layout_name)
+    main_process_func(layout_name, input_file_path, taskid)
+    task_status[taskid] = "completed"
+
+@app.post("/process_image_api/{layout_name}")
+# async def main_process(layout_name: str, file: UploadFile = File(...), background_tasks: BackgroundTasks):
+async def main_process(layout_name: str, file: UploadFile, background_tasks: BackgroundTasks):
+    input_file_path = f"temp_{file.filename}"
+    
+    with open(input_file_path, "wb") as buffer:
+        buffer.write(await file.read())
+    
+    unix_time = datetime.now(timezone.utc).timestamp() * 1000
+    taskid = f"{unix_time}.csv"
+    
+    # Изначально устанавливаем статус задачи как "in_progress"
+    task_status[taskid] = "in_progress"
+    background_tasks.add_task(run_main_process, layout_name, input_file_path, taskid)
+
+    return {
+        "message": "Task received, processing in background",
+        "taskid": taskid
+    }
+
+@app.get("/task_status/{taskid}")
+async def get_task_status(taskid: str):
+    status = task_status.get(taskid)
+    if status:
+        return {"taskid": taskid, "status": status}
+    else:
+        raise HTTPException(status_code=404, detail="Task not found")
+
+@app.get("/download_coords/{filename}")
+async def download_coords(filename: str):
+    return FileResponse(filename)
