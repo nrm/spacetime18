01311036c45d09ee02e53a4c36b1fe6e99c0c181
Bezrukov Ilya <bezrukov@iaaras.ru>
2024-06-22 23:52:28 +0400

    Merge add run test in multiply gitlab-runners

--- run_test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd ~/project
-python3 -m venv venv
-source venv/bin/activate
-pip install -r requirements.txt
-python test.py --layout /opt/spacetime/layouts/layout_2021-08-16.tif --crop /opt/1_20/crop_0_0_0000.tif
-cp result.csv ~/
