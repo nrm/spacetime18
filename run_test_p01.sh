cd8a068c94d7635dbbd074765d0b825b519fd442
Bezrukov Ilya <bezrukov@iaaras.ru>
2024-06-22 18:05:15 +0400

    multiple test version 0.01

---
+++ run_test_p01.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+taskid=$1
+host=$(hostname -s)
+result="result_${taskid}_${host}.csv"
+
+
+cd ~/project
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+# source ~/venv/bin/activate
+
+l = "/opt/spacetime/layoutslayout_2021-06-15.tif"
+for c in $(ls /opt/spacetime/1_20/*_0000.tif):
+do
+        echo "L is $l; C is $c";
+        python test.py --layout ${l} --crop ${c} --taskid ${result}
+done
+
+# cp result.csv ~/
+cp ${result} ~/
+echo "End"
--- test.py
+++ test.py
@@ -2,21 +2,17 @@ import argparse
 from process_image import main_process_func
 from datetime import datetime, timezone
 
-# Получение текущего времени в формате unix time
-# unix_time = datetime.now(timezone.utc).timestamp() * 1000
-# taskid = str(unix_time) + '.csv'
-
-# timestamp = datetime.now(timezone.utc).timestamp()
-# taskid = f"{int(timestamp * 1_000_000)}"
-taskid = "result.csv"
 
 # Создание парсера аргументов
 parser = argparse.ArgumentParser(description='Process some images.')
 parser.add_argument('--layout', type=str, required=True, help='Path to the layout file')
 parser.add_argument('--crop', type=str, required=True, help='Path to the input file')
+parser.add_argument('--taskid', type=str, required=True, help='taskid, name result files with extension')
 
 # Парсинг аргументов
 args = parser.parse_args()
 
+# taskid = "result.csv"
+
 # Вызов основной функции с аргументами
-main_process_func(args.layout, args.crop, taskid)
+main_process_func(args.layout, args.crop, args.taskid)
