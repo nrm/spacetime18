a4d547f68bef704b8f1c474b6cb103a0df965239
Bezrukov Ilya <bezrukov@iaaras.ru>
2024-06-23 00:00:28 +0400

    multiple test version 0.2. run_test

--- run_test_L.1.sh
+++ run_test_L.1.sh
@@ -24,11 +24,6 @@ files=(
     # "/opt/spacetime/1_20/crop_0_2_0000.tif"
     # "/opt/spacetime/1_20/crop_0_3_0000.tif"
     # "/opt/spacetime/1_20/crop_1_0_0000.tif"
-    # "/opt/spacetime/1_20/crop_1_1_0000.tif"
-    # "/opt/spacetime/1_20/crop_1_2_0000.tif"
-    # "/opt/spacetime/1_20/crop_1_3_0000.tif"
-    # "/opt/spacetime/1_20/crop_2_0_0000.tif"
-    # "/opt/spacetime/1_20/crop_2_1_0000.tif"
 )
 
 for crop_file in "${files[@]}"
