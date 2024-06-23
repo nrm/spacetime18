41baff37a39b7c97aa78079a1b758bf61ae5671f
ksl <kurdubov@iaaras.ru>
2024-06-23 06:55:42 +0300

    run_test with ISmult=4

--- process_image.py
+++ process_image.py
@@ -683,7 +683,7 @@ def process_crop(crop, crop_file_name, substrate, mults, refined_mults, method='
     #diff_crop[0, :]=np.zeros(diff_crop.shape[1])
     diff_crop = make_derivative(med_crop,1,1,deriv_type)
 
-    ISmult=2
+    ISmult=4
     if ISmult!=1:
         med_crop_sm = smooth(med_crop,ISmult,ISmult)      
     else:
